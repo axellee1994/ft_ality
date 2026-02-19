fc5ebf60fc235aea6e66da2914ba86481bc7044e
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2026-02-19 20:23:37 +0800

    used trampolin method for recursion

--- src/interactive.py
+++ src/interactive.py
@@ -3,33 +3,39 @@
 import sys
 import tty
 import termios
-from typing import Tuple
+from typing import Tuple, Callable, Optional
 from .utils import Grammar, Automaton
 from .recognition import process_key_input
+
+
+def trampoline(thunk: Callable) -> None:
+    """Drive tail-recursive thunks without growing the call stack"""
+    result = thunk
+    while callable(result):
+        result = result()
 
 
 def run_interactive_mode(
     grammar: Grammar, automaton: Automaton, debug: bool = False
 ) -> None:
     """Run interactive recognition mode"""
-    print("\nTraining Mode")
+    print("\nGame Training/Tutorial Mode")
     print("Press keys to execute moves (Ctrl+C to exit)\n")
 
     def read_key() -> str:
         return sys.stdin.read(1)
 
-    def loop(buffer: Tuple[str, ...]) -> None:
+    def loop(buffer: Tuple[str, ...]) -> Optional[Callable]:
         key = read_key()
         if not key:
-            return
+            return None
         new_buffer, _ = process_key_input(key, grammar, automaton, buffer, debug)
-        loop(new_buffer)
+        return lambda: loop(new_buffer)
 
-    sys.setrecursionlimit(10000)
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setcbreak(fd)
-        loop(())
+        trampoline(lambda: loop(()))
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
